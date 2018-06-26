ab7d23a5919e247bf59f4f51f736178649ce3931
Muhammad Abdulmalik <makhirmjd@gmail.com>
2018-06-26 22:52:45 +0100

    Demonstrated JVM Test

---
+++ app/src/test/java/com/sqisland/tutorial/recipes/data/model/RecipeTest.java
@@ -0,0 +1,29 @@
+package com.sqisland.tutorial.recipes.data.model;
+
+import org.junit.Test;
+
+import java.io.InputStream;
+
+import static org.junit.Assert.*;
+
+public class RecipeTest {
+    @Test
+    public void water() {
+        InputStream stream = RecipeTest.class.getResourceAsStream("/recipes/water.txt");
+        Recipe recipe = Recipe.readFromStream(stream);
+        assertNotNull(recipe);
+        assertEquals("water", recipe.id);
+        assertEquals("Water", recipe.title);
+        assertEquals("Put glass under tap. Open tap. Close tap. Drink.", recipe.description);
+    }
+
+    @Test
+    public void punch() {
+        InputStream stream = RecipeTest.class.getResourceAsStream("/recipes/mixed.txt");
+        Recipe recipe = Recipe.readFromStream(stream);
+        assertNotNull(recipe);
+        assertEquals("punch", recipe.id);
+        assertEquals("Punch", recipe.title);
+        //assertEquals("Put glass under tap. Open tap. Close tap. Drink.", recipe.description);
+    }
+}
